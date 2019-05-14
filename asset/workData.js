99b410ee1fed64f04a4e01c2a60552078284a963
jiayu Lu <lovesyndra@gmail.com>
2019-05-15 00:45:34 +0800

    0515 data

---
+++ asset/workData.js
@@ -0,0 +1,681 @@
+const workData = [
+	{
+		id: 1,
+		location: '8sip18second',
+		name: '8口分之18秒',
+		person: ['鄭心怡', '蕭雨濛'],
+		facepic:['person_1','person_2'],
+		info: '現代人在超商選擇即食食品的比例高，為了節省時間，食品包裝通常方便開啟，進而養成了消費者狼吞虎嚥的飲食習慣。為解決現代人飲食不規律的問題，本作品運用包裝裂線的撕與折來開啟產品。容器上特殊的孔洞設計，促使食物停在一口的份量。包裝內側呈現每種食物應分成幾口食用、咀嚼次數、費時多久等飲食資訊。讓消費者能有效率且健康的食用速食，提醒現代人要樂活享食。',
+		content: `
+		<div class="bigpic">
+			<img src='asset/8sip18second/1.jpg' />
+		</div>
+		<p class="text">
+			主題概念將食品包裝設計平均分為8口食用，每口花費18秒咀嚼，故命名為8口分之18秒，傳達本專題之理念。
+		</p>
+		<p class="text">
+		本商品包裝設計作品之研究在於顛覆以往的速食包裝結果，以反便利性為出發點，提倡國民健康有效率的飲食。藉由現有包裝設計進行改良，利用裂線和特殊孔洞的設計，開啟食物包裝時，能停在一口的份量，包裝內側呈現商品資訊，使產品使用者能細嚼慢嚥，用最有效率及健康的咀嚼次數和時間來食用。
+		</p>
+		<div class="bigpic">
+			<img src='asset/8sip18second/3.jpg'/>
+		</div>
+		<p class="text">
+		握便當包裝設計 - 扁平式的包裝，在撕裂線時會在左側停下一口的量。
+		</p>
+		<p class="text">
+		手卷、三明治包裝設計 - 以撕的方式開啟食品．包裝上設計特殊孔洞，撕的時候能剛好停在一口的份量。
+		</p>
+		<p class="text">
+		熱狗袋包裝設計 - 以折的概念，紙袋沿著設計好的折痕，折一次為一口的量。
+		</p>
+		`,
+		videoOpenning: null,
+		videoEndding: {
+			location: '2.gif',
+			youtubeSrc: 'lgHaCoXWvhA',
+		}
+	},
+	{
+		id: 2,
+		location: 'anorangewish',
+		name: '柑願',
+		person: ['林涵茜', '廖嘉暄','劉于安' ],
+		facepic:['person_1','person_2', 'person_3'],
+		info: '由外，客家藍色調及酸柑表皮紋理之設計；而內，酸柑茶繁複製作程序的繪製，以這些元素，將客家永恆的精神「刻苦耐勞」包覆在裡頭。這份酸柑茶喝的不僅是文化，更是傳承。',
+		content: `
+		<div class="bigpic">
+			<img src='asset/anorangewish/1.jpg' />
+		</div>
+		<h2 class="subtitle">
+		酸柑茶
+		</h2>
+		<p class="text">
+			是台灣客家特有的文化，也是台灣唯一的緊壓茶。將年後滯銷的虎頭柑收集再利用，把它挖空後塞入混合果肉的茶葉，再透過繁複的九道程序「採、開、填、綁、蒸、壓、曬、烘、養」來製作成酸柑茶。九蒸九曬，繁複的製作程序，體現了客家人克勤克儉的精神。
+		</p>
+		<div class="pic">
+		<img src='asset/anorangewish/2.jpg'/>
+		</div>
+		<p class="text">
+		整套茶禮盒主要想推廣酸柑茶這項台灣客家特有的文化與精神，而搭配上「公道杯」原理的茶具設計，是希望讓更多想體驗泡茶樂趣的初學者或是繁忙的上班族能用簡單的方式泡好茶。
+		</p>
+		<div class="bigpic">
+			<img src='asset/anorangewish/3.jpg'/>
+		</div>
+		`,
+		videoOpenning: null,
+		videoEndding: null
+	},
+	{
+		id: 3,
+		location: 'banjon',
+		name: '包中',
+		person: ['蔡季玲'],
+		facepic:['person_1'],
+		info: '以人們祈求神明協助達成願望為發想的方向，以飲用包種茶為推廣為目的，茶葉品種的中文名稱與願望達成的中文音調相同，紅色與能量相關，經常使用在鼓動、激勵的活動上，希望人們帶著產品去祭拜神明時，人人都能心想事成。',
+		content: `
+		<div class="pic">
+			<img src='asset/banjon/1.jpg'/>
+		</div>
+		<div class="pic">
+			<img src='asset/banjon/2.jpg'/>
+		</div>
+		<p class="text">
+			內容物共有五個小包裝，都是以包子的外形為設計發想來源，整體的大包裝則是以中國傳統的蒸籠外形為概念，內容物的五種中藥材，分別區分成五個個品項,象徵著祈求考試上榜、祈求升官、祈求婚姻、祈求事業順利及祈求中奬發財等五種不同的意義。
+		</p>
+		<div class="pic">
+			<img src='asset/banjon/3.jpg'/>
+		</div>
+		<p class="text">
+			運用包種茶結合天然的中藥材，分別像徵著五種不同願望的祈求，並將願望寫在祈求卡上，一起向眾神禱告，飲用禱告後的中藥材所混合的荼品，並隨身攜帶著祈求卡，將可等待著夢想成真的到來。
+		</p>
+		`,
+		videoOpenning: null,
+		videoEndding: null
+	},
+	{
+		id: 4,
+		location: 'bendingdown',
+		name: '彎下腰',
+		person: ['林盈秀', '徐梓芫', '徐維辰', '張惠瑄',],
+		facepic:['person_1', 'person_2', 'person_3', 'person_4',],
+		info: '泰山區貴子里的一群人，安安靜靜地潔淨鄉土，就像農人彎下腰來插秧似的，把愛種回所屬的大地，不求回報。我們想說他們的故事，讓土地充滿更多如是美麗的身影。',
+		content: `
+		<div class="pic">
+			<img src='asset/bendingdown/1.jpg'/>
+		</div>
+		<h2 class="subtitle">
+			刊物設計
+		</h2>
+		<p class="text">
+			貴子里的志工們居多是中老年人，在我們加入後發現有一位阿公今年已經93歲了，他不管風吹日曬雨淋每天準時五點出勤掃地，他愛土地的心真的讓我們非常感動！希望藉由此書分享貴子里的美好。
+		</p>
+		<div class="pic">
+			<img src='asset/bendingdown/2.jpg'/>
+		</div>
+		<h2 class="subtitle">
+			菸蒂回收筒
+		</h2>
+		<p class="text">
+			在貴子里的道路上都有設置菸蒂回收筒提供行人丟棄煙蒂外，也利於志工們清潔，但罐子卻時常被當成垃圾桶，所以我們重新設計了視覺，以及罐子側邊的QR code可以掃描到網站讓志工即使在外也可以方便簽到，順便也解決了使用紙本產生的人力問題。
+		</p>
+		<div class="pic">
+			<img src='asset/bendingdown/3.jpg'/>
+		</div>
+		<h2 class="subtitle">
+			網頁設計
+		</h2>
+		<p class="text">
+			行動式的網站也包含了一些志工專屬資訊及故事專欄分享區，我們希望這群美麗的身影可以讓更多人看見。
+		</p>
+		`,
+		videoOpenning: null,
+		videoEndding: null
+	},
+	{
+		id: 5,
+		location: 'diamondsutra',
+		name: 'DiamondSutra剛',
+		person: ['曾珮禎', '曾靜慧', '黃尹琳', '盧佳瑜',],
+		facepic:['person_1', 'person_2', 'person_3', 'person_4',],
+		info: '佛經長篇累牘，若無引介者諄諄教導，自讀者容易卻步，感受不到經典的魅力。我們從《金剛經》擷取偈子，以視覺、互動的方式呈現，讓讀者輕鬆親近經典。',
+		content: `
+		<div class="bigpic">
+			<img src='asset/diamondsutra/1.jpg'/>
+		</div>
+		<p class="text">
+			選取金剛經裡四十句偈子，作為我們專題的主要結構，配合偈子的內容作畫面設計及互動安排，達到拓廣閱讀年齡層，使讀者可以不因深奧的內文因此錯過此優良讀品。
+		</p>
+		<div class="pic">
+			<img src='asset/diamondsutra/2.jpg'/>
+		</div>
+		<h2 class="subtitle">
+			拼圖設計
+		</h2>
+		<p class="text">
+			拼圖有使人思考及視線駐留在畫面上的時間較長的特性，在讀者尋找對應的圖面時，可以在原文和圖片觀看時間拉長，讀者在不知不覺中能對偈子有印象卻能不帶負面情緒。
+		</p>
+		<div class="pic">
+			<img src='asset/diamondsutra/3.jpg'/>
+		</div>
+		<div class="pic">
+			<img src='asset/diamondsutra/4.jpg'/>
+		</div>
+		<h2 class="subtitle">
+			互動網站
+		</h2>
+		<p class="text">
+			首頁為偈子常見閱讀編排，讀者拼湊完後使用AR掃描後可以再次看到完整偈子藉此加深記憶，而小遊戲的設計由對應的偈子意義去創作，例如:當生如是心，我應滅度一切眾生。滅度一切眾生已，而無有一眾生時滅度者。此句的解釋為如一切煩惱、妄想、取捨、貪嗔、忌妒、人我四相等，佛等應一一為之滅度，所謂無欲，就能住心，亦能降伏妄心。我所謂滅度者，不過指點出真性情，使之自悟。
+		</p>
+		<div class="pic">
+			<img src='asset/diamondsutra/5.jpg'/>
+		</div>
+		<p class="text">
+			取用去相的元素去設計互動網頁，在讀者經過拼圖、首頁再來到互動設計時經過多層次的思考，進而進入偈子解釋的頁面也不會覺得突然而感到唐突，而是循序漸進的進入金剛經內部。
+		</p>
+		<div class="bigpic">
+			<img src='asset/diamondsutra/6.jpg'/>
+		</div>
+		`,
+		videoOpenning: null,
+		videoEndding: {
+			location: '2.jpg',
+			youtubeSrc: '5d6VACBOyKQ',
+		},
+	},
+	{
+		id: 6,
+		location: 'exquisite',
+		name: '金漬',
+		person: ['莊雯淇', '林承慧', '陳姿穎', '趙致本',],
+		facepic:['person_1', 'person_2', 'person_3', 'person_4',],
+		info: '「金漬」是推廣台灣有機醬菜的品牌，挑選各地有機食材作為產品。視覺呈現出純樸、天然意象，結合台灣早期特色，展現出屬於台灣味的醬菜。',
+		content: `
+		<div class="pic">
+			<img src='asset/exquisite/1.jpg'/>
+		</div>
+		<h2 class="subtitle">
+			包裝設計
+		</h2>
+		<p class="text">
+			台灣早期沒有塑膠袋的時代，人們會運用布包裹住物品來做為運送方式，所以我們採用布包的形式作為主包裝呈現，自然樸實的外在，更能貼近家鄉味道。
+		</p>
+		<div class="pic">
+			<img src='asset/exquisite/2.jpg'/>
+		</div>
+		<p class="text">
+			而在台灣早期交通不便利、資訊不發達的生活中，火車成了最常見的交通工具，也是人們聯繫感情的橋樑，我們則運用火車票的形式結合醬菜上的吊牌，更能勾勒出時代的回憶。
+		</p>
+		<div class="bigpic">
+			<img src='asset/exquisite/3.jpg'/>
+		</div>
+		<div class="pic">
+			<img src='asset/exquisite/4.jpg'/>
+		</div>
+		<div class="pic">
+			<img src='asset/exquisite/5.jpg'/>
+		</div>
+		<div class="pic">
+			<img src='asset/exquisite/6.jpg'/>
+		</div>
+		`,
+		videoOpenning: null,
+		videoEndding: null
+	},
+	{
+		id: 7,
+		location: 'fragrantunderstanding',
+		name: '香識',
+		person: ['方毓萱', '徐萓穗', '莊姿恩', '蔡艾臻',],
+		facepic:['person_1', 'person_2', 'person_3', 'person_4',],
+		info: '「金漬」是推廣台灣有機醬菜的品牌，挑選各地有機食材作為產品。視覺呈現出純樸、天然意象，結合台灣早期特色，展現出屬於台灣味的醬菜。',
+		content: `
+		<p class="text">
+			以介紹香料嗅覺為主軸，利用插畫取代以往食譜書的照片形式，以五種香料:迷迭香、肉桂、辣椒、胡椒、八角為主角，每一種香料選用五道料理繪製成食譜書。
+		</p>
+		<div class="pic">
+			<img src='asset/fragrantunderstanding/1.jpg'/>
+		</div>
+		<h2 class="subtitle">
+			食譜書
+		</h2>
+		<p class="text">
+			在封面縫上親手製作的香料手抄紙，刻意不將香料打得很碎，留下香料本身些許樣貌，讓大家可以在觀看食譜書時，也能聞到香料本身的味道，在視覺、嗅覺、觸覺上一次滿足。
+		</p>
+		<div class="pic">
+			<img src='asset/fragrantunderstanding/2.jpg'/>
+		</div>
+		<h2 class="subtitle">
+			內頁
+		</h2>
+		<p class="text">
+			將烹調料理的步驟和工廠運作的方式做結合，以插畫方式表現，增加趣味性。引領大家順著平檯、運送帶及階梯知道料理下一 步該進行的步驟。
+		</p>
+		<div class="pic">
+			<img src='asset/fragrantunderstanding/3.jpg'/>
+		</div>
+		<div class="pic">
+			<img src='asset/fragrantunderstanding/4.jpg'/>
+		</div>
+		`,
+		videoOpenning: null,
+		videoEndding: null
+	},
+	{
+		id: 8,
+		location: 'frankenstein',
+		name: '弗婪肯斯坦',
+		person: ['林昇陽', '張芯瑜', '陳佑', '黃純瑜',],
+		facepic:['person_1', 'person_2', 'person_3', 'person_4',],
+		info: '為了心中那份執念，而墮入瘋狂，踐踏別人的生命也在所不惜。人可以為了自己的理想，犧牲多少東西呢？即使已經跨越所謂道德和倫理的界線。小女孩誤闖了一棟房子，每個房間都有奇異的藝術品，闡述自身被創造時藝術家的狀態，逐步揭開他不為人知的偏執，藝術的美和現實交錯，讓人陷入瘋狂，誰能確定此時此刻，自己是否正常呢？',
+		content: ``,
+		videoOpenning: null,
+		videoEndding: {
+			location: 'main.jpg',
+			youtubeSrc: '5jzWXorqS5g',
+		},
+	},
+	{
+		id: 9,
+		location: 'guanlaisianne',
+		name: '諺來如此',
+		person: ['阮宜婷', '蔡雲筑', '顏郁文', '賴怡雯', '廖珮鈞'],
+		facepic:['person_1', 'person_2', 'person_3', 'person_4', 'person_5'],
+		info: '小時候，阿公、阿嬤常與我們用閩南語對話，開始唸書後，課綱都是中文，教我們成語、文言文⋯等，閩南語的使用上大量減少，所以，我們藉由動物的諺語加上可愛風格，讓更多小朋友認識閩南語諺語，其實比成語更有意思跟想法。',
+		content: `
+		<div class="pic">
+			<img src='asset/guanlaisianne/1.jpg'/>
+		</div>
+		<h2 class="subtitle">
+			字卡難度等級介紹
+		</h2>
+		<p class="text">
+			綠色字卡代表「簡易級」，小朋友可以先從此級別開始練習！
+		</p>
+		<div class="pic">
+			<img src='asset/guanlaisianne/2.jpg'/>
+		</div>
+		<p class="text">
+			藍色字卡代表「中度級」，在熟悉「簡易級（綠色）」字卡後，進階之練習。
+		</p>
+		<div class="pic">
+			<img src='asset/guanlaisianne/3.jpg'/>
+		</div>
+		<p class="text">
+			紅色字卡代表「困難級」，分別熟悉「簡易（綠色）級」及「中度（藍色）級」的字卡後，進階練習難度最高的字卡！
+		</p>
+		`,
+		videoOpenning: null,
+		videoEndding: null
+	},
+	{
+		id: 10,
+		location: 'hoarding',
+		name: '囤',
+		person: ['陳宣兆', '陳捷羽', '廖婉婷', '鄭羽倢'],
+		facepic:['person_1', 'person_2', 'person_3', 'person_4'],
+		info: '這片土地已滿載。在這生產過剩的時代裡，我們皆是囤物者。時間快速挪移，讓人漸失自覺；空間用來囤物卻誤以為擁有。透過囤積物此主題，探討物品、空間、情感三者間的關係。',
+		content: `
+		<div class="pic">
+			<img src='asset/hoarding/1.jpg'/>
+		</div>
+		<div class="pic">
+			<img src='asset/hoarding/2.jpg'/>
+		</div>
+		<div class="pic">
+			<img src='asset/hoarding/3.jpg'/>
+		</div>
+		<div class="pic">
+			<img src='asset/hoarding/4.jpg'/>
+		</div>
+		`,
+		videoOpenning: null,
+		videoEndding: null
+	},
+	{
+		id: 11,
+		location: 'innerlights',
+		name: '藏芒',
+		person: ['陳翊左', '黃靜瑄', '陳明鍠'],
+		facepic:['person_1', 'person_2', 'person_3'],
+		info: '在中華文化中，「禮」的學問廣泛，贈刀禮是其中一個蘊含深厚寓意的禮節。古代歷史中，以刀贈友代表對朋友的信任和重視，刀可護亦可殺，如同將性命交付於他人之手，意味彼此為生死之交。若是上級者贈送給下屬，則代表對其之器重期許之意；下級贈予上司時，代表臣服與忠誠，少數民族中也有男女以刀為定情信物、兄弟以刀結義等作為感情溝通的媒介。',
+		content: `
+		<div class="bigpic">
+			<img src='asset/innerlights/1.jpg'/>
+		</div>
+		<p class="text">
+			「藏芒」意為藏匿光芒，待重要時刻當機立斷，由於刀同時具有危險與保護性，因此將理念投射於作品中，意為重要人事物才使其刀出鞘。<br/>
+			刀是我們呈現生命力的一種載體，水深火熱極端的反差，如我們所經歷過的淬煉，鍛打與研磨是試煉的過程，除去外表，變得平滑且細緻，拋光後的反光是努力後的成果，映出內心的堅毅剛直，象徵製刀精神的昇華。刀之所以為刀，乃是經淬火, 方成鋼刀，唯經歷極端的淬煉，還能挺直腰桿，才是刀的真諦。
+		</p>
+		<div class="bigpic">
+			<img src='asset/innerlights/2.jpg'/>
+		</div>
+		<h2 class="subtitle">
+			設計理念
+		</h2>
+		<p class="text">
+			以餘料新造手工刀具，刀身保留原始炭皮，裝具搭配使用香氣木料與皮革的溫潤細緻，不僅限於視覺欣賞更貼近整體感官體驗，包裝高雅，與市面上木盒更具創新及收藏性。<br/>
+			自古以來，以刀贈友視為情感的傳遞，代表雙方信任與珍視，本團隊致力將禮的意涵提升至心靈層面。
+		</p>
+		<div class="bigpic">
+			<img src='asset/innerlights/3.jpg'/>
+		</div>
+		<h2 class="subtitle">
+			創作特色
+		</h2>
+		<p class="text">
+			傳統製刀技術與文化結合現代設計，以手工製作獨樹一格的刀具，分為四種系列：<br/>
+			「須彌」：梵語中極大之意，為本展品最大尺寸之系列。<br/>
+			「臥心」：取自臥薪嘗膽的臥薪，改為心字，期許能成為在您胸口，保護您的好夥伴。<br/>
+			「鍠」：原指古代的禮樂器，亦是兵器，能文善武，此系列不只於優雅外型，更兼具實用性。<br/>
+			「無諍」： 不執著於刀的外裝造型 將刀回歸簡樸 裸柄即可握持。<br/>
+			包裝以木牘做設計跳脫傳統木盒思維，以明鏡製造空間感。<br/>
+		</p>
+		<p class="text">
+			品牌主視覺使用中國傳統色與簡約線條呈現，以配置具有傳統中國色彩的風格，依序為火紅、火青、杏黃、深灰、淺灰。意味爐火純青、拙鐵成金。以刀切斜面發想，搭配木紋曲線，色塊分明，營造柔和卻又簡潔俐落的視覺感。搭配木材之特殊香氣，令人放鬆紓壓，皮革製作刀鞘及傘繩交織成繩柄，除了具有收藏賞玩及贈與價值，也可於生活之中隨身攜帶運用。<br/>
+			以傳承、環保與再利用為主要概念，所製作之產品，皆以邊材餘料製作，避免資源浪費。
+		</p>
+		<div class="bigpic">
+			<img src='asset/innerlights/4.jpg'/>
+		</div>
+		<h2 class="subtitle">
+			延續與傳承
+		</h2>
+		<p class="text">
+			在機器生產尚未普及的時代，以手工與簡便的器具所製造的刀具，是經驗和工藝累積的結晶，每件作品都獨一無二，本品牌期許能延續刀的文化，使傳統工藝產生新價值，將文化與技術傳承，加入藝術與創作，提供喜愛設計、偏好獨特風格的群眾贈禮和收藏。傳承不只於刀具本身，後續將開授體驗課程，讓民眾可自行設計製作屬於自己獨創的刀具，體會手工刀具的製程與藝術文化，延續品牌概念。
+		</p>
+		`,
+		videoOpenning: null,
+		videoEndding: null
+	},
+	{
+		id: 12,
+		location: 'pickupstone',
+		name: '拾石',
+		person: ['王嘉鴻', '柯威成', '施妙姍', '彭立鋐', '鄭宇軒'],
+		facepic:['person_1', 'person_2', 'person_3', 'person_4', 'person_5'],
+		info: '礦見台灣原石之美，由最原始的風貌去延伸出拾石的初步概念，一切由這個動作開始，四面環海的地利位置所發想，構出最自然的風貌，我們想將最純粹的撿拾，構出最自然的風貌，不僅僅只是觀賞，更具有不同的故事，引人入勝，銀飾不單單只是飾品，更是一段直得回味的精華。',
+		content: `
+		<div class="bigpic">
+			<img src='asset/pickupstone/1.jpg'/>
+		</div>
+		<h2 class="subtitle">
+			作品概念來自原礦
+		</h2>
+		<p class="text">
+			原礦在切割之前都是在原石裡，在石頭紋理的銀飾上鑲嵌原礦做成戒指、項鍊、耳環，象徵原礦是隱藏在石頭的核心。
+		</p>
+		<div class="bigpic">
+			<img src='asset/pickupstone/2.jpg'/>
+		</div>
+		<h2 class="subtitle">
+			石頭從何而來？
+		</h2>
+		<p class="text">
+			石頭大多數是由山脈與河川、海岸挖掘而來，木盒戒指包裝外層包裹山與水造型的銀片，讓兩種不同材質產生特殊感受。層層關係，銀飾將原礦包裹，包裝將銀飾包裹，原礦從石頭來，石頭從山水來，作品間的相互連結。
+		</p>
+		<div class="pic">
+			<img src='asset/pickupstone/3.jpg'/>
+		</div>
+		<div class="pic">
+			<img src='asset/pickupstone/4.jpg'/>
+		</div>
+		`,
+		videoOpenning: null,
+		videoEndding: null
+	},
+	{
+		id: 13,
+		location: 'redbrickstroy',
+		name: '磚語',
+		person: ['曾冠佳', '黃于耀', '鄭宇翔'],
+		facepic:['person_1', 'person_2', 'person_3'],
+		info: '在過去，用紅磚堆砌起的房屋處處可見，但如今卻不易尋見。為了蓋水泥大樓，我們拆除的不僅是舊建材房子，更是老一輩對家的回憶。因此我們希望可以透過影片紀錄來為這些50年代的長輩們守護家的記憶與情懷。',
+		content: `
+		<div class="pic">
+			<img src='asset/redbrickstroy/1.jpg'/>
+		</div>
+		<h2 class="subtitle">
+			順達窯業
+		</h2>
+		<p class="text">
+			順達窯業從民國54年開始從事紅磚的製造生產，創立之初，由單一的八卦窯，雙八卦窯的人工生產紅磚，成長到今日的全自動隧道窯，從原料土胚的處理，粗胚的成型，台車疊胚，到紅磚的燒成，讓民眾見證歷史的遺跡。
+		</p>
+		<div class="pic">
+			<img src='asset/redbrickstroy/2.jpg'/>
+		</div>
+		<div class="bigpic">
+			<img src='asset/redbrickstroy/3.jpg'/>
+		</div>
+		`,
+		videoOpenning: null,
+		videoEndding: null
+	},
+	{
+		id: 14,
+		location: 'ridiculousexcousenews',
+		name: '藉口報報',
+		person: ['李依庭', '李遠媛', '張瑜臻', '陳子婷', '陳孟平', '羅丹妤'],
+		facepic:['person_1', 'person_2', 'person_3', 'person_4', 'person_5', 'person_6'],
+		info: '「抱歉遲到了，我在捷運上塞車！」在這個人與人相處頻繁的社會當中，每天的互動是不可避免的。當我們遇到不想面對的事，就會找理由說服自己、說服別人，沒錯！這就是「藉口」。藉口像是生活必需品，不講好像沒辦法，於是我們只好『不斷找藉口，讓人生向前走』。',
+		content: `
+		
+		<div class="bigpic">
+			<img src='asset/ridiculousexcousenews/1.jpg'/>
+		</div>
+		<p class="text">
+			「報紙」是日常必需品、且為傳播媒體的一種，因此我們借之作為呈現的媒介。新聞常以聳動且誇大的標題吸引大眾點閱，但內容往往圖文不符或與事實相反；我們將新聞內容，以藉口結合歷史名人所發生的大小事件，重新創造、加以荒謬化，並且在乏味的圖文上覆蓋無厘頭塗鴉，反射媒體對真相之掩蓋、誇大與不實。
+		</p>
+		<p class="text">
+			以每週一爆料的「週報」形式出版，以2020年為基準，當年度共五十三週，因此將呈現五十三篇作品。此專題以「療癒性視覺設計」為主軸，希望我們的設計能夠讓觀賞者會心一笑，舒緩生活壓力。
+		</p>
+		<div class="bigpic">
+			<img src='asset/ridiculousexcousenews/2.jpg'/>
+		</div>
+		<h2 class="subtitle">Q & A</h2>
+		<p class="text">
+			Q:為什麼用歷史人物？<br/>
+			A:是人都會講藉口，會被放大檢視、被新聞拿來報導的，都是「名人」。舉例之前某議員在助選時被拍到翻白眼，事後卻說是「在做眼球運動」，甚至教導大家平時如何保養眼睛。
+		</p>
+		<p class="text">
+			Q:為什麼要在上面塗鴉？<br/>
+			A:國高中時期，在國文、歷史課上課無聊時，我們會將課本上的歷史名人塗鴉改造；只有上半身人物，將他們繪製成有身體的搞笑插畫。作品結合這個方式，讓報紙搖身一變，增加趣味、且反射媒體對事實的誇大或掩蓋。
+		</p>
+		`,
+		videoOpenning: null,
+		videoEndding: null
+	},
+	{
+		id: 15,
+		location: 'ssuhsiutsai',
+		name: '四秀仔',
+		person: ['高詩婷'],
+		facepic:['person_1'],
+		info: '因為現代人幾乎都成了文明病下的魁儡，都只透過沒有感情的打字方式來傳遞訊息，造就成了現代人之間距離疏遠的情感，我想藉由這三層系列的設計的組合，希望傳達著彼此分享，就像是分享生活與心情，拉近人與人之間的距離。',
+		content: `
+		<div class="pic">
+			<img src='asset/ssuhsiutsai/1.jpg'/>
+		</div>
+		<p class="text">
+			可放四種不同的零食台灣話則稱四秀仔。
+		</p>
+		<div class="pic">
+			<img src='asset/ssuhsiutsai/2.jpg'/>
+		</div>
+		<div class="pic">
+			<img src='asset/ssuhsiutsai/3.jpg'/>
+		</div>
+		<div class="pic">
+			<img src='asset/ssuhsiutsai/4.jpg'/>
+		</div>
+		`,
+		videoOpenning: null,
+		videoEndding: null
+	},
+	{
+		id: 16,
+		location: 'thesunnyshow',
+		name: 'The Sunny Show',
+		person: ['陳品錞', '陳映瑄', '陳筱雙', '簡靖芸', '魏敏'],
+		facepic:['person_1', 'person_2', 'person_3', 'person_4', 'person_5'],
+		info: '故事描述台灣家庭裡的一位小女孩與父親間發生隱私權衝突問題。',
+		content: `
+		<div class="pic">
+			<img src='asset/thesunnyshow/2.jpg'/>
+		</div>
+		<div class="pic">
+			<img src='asset/thesunnyshow/3.jpg'/>
+		</div>
+		<div class="pic">
+			<img src='asset/thesunnyshow/4.jpg'/>
+		</div>
+		<div class="pic">
+			<img src='asset/thesunnyshow/5.jpg'/>
+		</div>
+		<div class="pic">
+			<img src='asset/thesunnyshow/6.jpg'/>
+		</div>
+		`,
+		videoOpenning: {
+			location: '1.jpg',
+			youtubeSrc: 'clNPLBhn9eA',
+		},
+		videoEndding: null
+	},
+	{
+		id: 17,
+		location: 'truth',
+		name: '都給你說就好啦！',
+		person: ['余絃', '簡貝姍'],
+		facepic:['person_1', 'person_2'],
+		info: '當你成為設計人後，累積了多少委屈，熬了多少的夜，換來的全是老師的退件與老闆、客戶的否定。在我們創造的世界裡，沒有堅持！不做設計！全照他們說的做。',
+		content: `
+		<div class="pic">
+			<img src='asset/truth/1.jpg'/>
+		</div>
+		<p class="text">
+			我們目標族群以設計人為出發點，將設計人內心的小小世界做成網頁互動式遊戲。不管是在學校還是職場，一定都會遇到各種心裡想要抱怨的情境，我們將這些情境設計成具趣味性、帶有諷刺意味的遊戲關卡，讓遊玩者體驗曾經的崩潰時刻！那現在就讓我們來看我們的網頁呈現吧！
+		</p>
+		<div class="pic">
+			<img src='asset/truth/2.jpg'/>
+		</div>
+		<h2 class="subtitle">
+			你知道嗎？
+		</h2>
+		<p class="text">
+			在別人眼中，設計系的穿搭都很不一樣，來選擇一個你的造型吧！<br/>
+			為了凸顯每位設計人的獨特性，我們特別設計了可以自訂角色造型的區塊，讓遊玩者可以自由使用我們配置好的物件，隨機搭配自己想要的角色造型。
+		</p>
+		<div class="pic">
+			<img src='asset/truth/3.jpg'/>
+		</div>
+		<div class="bigpic">
+			<img src='asset/truth/4.jpg'/>
+		</div>
+		<h2 class="subtitle">
+			開始遊戲
+		</h2>
+		<p class="text">
+			首先，需要選擇您想要闖關的關卡石板，這裡設計的概念並沒有分學校或是職場，主要以主題式設計。例如：「老闆的排版堅持、客戶的美感…我到底誤會了什麼？」等等，遊戲內的畫面都是我們經由問卷調查、蒐集，篩選出會讓設計人感覺較為崩潰、抱怨、厭世的情境。接下來就可以依照關卡規則自行遊玩囉！
+		</p>
+		<div class="pic">
+			<img src='asset/truth/5.jpg'/>
+		</div>
+		<h2 class="subtitle">
+			收藏圖鑑
+		</h2>
+		<p class="text">
+			每關關卡都有隱藏圖卡可以蒐集，圖卡都是根據該關卡的情境所繪製，並搭配一句有梗的話，做成類似於梗圖的概念，讓遊玩者增添破關尋寶的樂趣。來舉一個有趣的梗話例子吧！接到有關於修圖的案子或是客戶時，照片上原先的手臂或是頭頂的部分被截掉了等等，結果要求我們幫補回來消失的部分，而且認為PS一下很簡單吧！真的很想說：「我們又不是大體修復師！」。
+		</p>
+		<div class="pic">
+			<img src='asset/truth/6.jpg'/>
+		</div>
+		<h2 class="subtitle">
+			周邊商品
+		</h2>
+		<p class="text">
+			我們的周邊商品主要有主視覺宣傳海報，將各個關卡設計成明信片以及熱門商品─惡搞Adobe工具列滑鼠墊，將遊戲情境會使用到的小物件設計成icon，擺放得像設計人常接觸的Adobe工具列，以雷射雕刻在鋁合金滑鼠墊的形式製作而成，兼具惡趣味與美觀。最後還有我們將角色挨揍的表情設計為發洩用角色杯墊，熬夜、趕工時候可以使用馬克杯好好的捶打他們，舒緩一下情緒。
+			</p>
+		`,
+		videoOpenning: null,
+		videoEndding: {
+			location: '7.jpg',
+			youtubeSrc: 'En_SAvj9-Lg'
+		}
+	},
+	{
+		id: 18,
+		location: 'what',
+		name: '蝦米',
+		person: ['梁庭瑋', '郭靜樺', '許嘉晏', '黃芷卉'],
+		facepic:['person_1', 'person_2','person_3', 'person_4'],
+		info: '人在生活中需要一些新鮮感或驚奇，有一些人會覺得生活很無聊 ，但只是他們還沒有發現很多東西是他們可以去探索的。我們認為獲得新知能夠增加生活中的樂趣，所以我們將一些知識用更有趣的方式呈現，讓人們對一些他不曾感興趣的事物產生興趣。世界上很多知識都是用較為嚴肅的文字搭配一些真實的照片來表達，容易讓人不了解其有趣的地方在哪裡，人們也因此忽略了這些世界上神奇的事物，我們利用有趣的書籍和有聲裝置，用說故事的方式，讓人們在理解某個知識的同時，也發現世界的有趣與奇妙。',
+		content: `
+		<div class="pic">
+			<img src='asset/what/1.jpg'/>
+		</div>
+		<h2 class="subtitle">
+			書籍
+		</h2>
+		<p class="text">
+			為我們的主要呈現，每本書內容皆會以漫畫的方式敘述一個知識，故事內容的角色們都是演員，像演一個故事給人們看，漫畫也像戲劇一樣，有第一幕、第二幕、第三幕，到了故事的最後將會解說這是一個什麼樣的知識。書籍內頁以四折頁設計，為布的精裝書，另有書殼可以收納。
+		</p>
+		<div class="pic">
+			<img src='asset/what/2.jpg'/>
+		</div>
+		<h2 class="subtitle">
+			裝置
+		</h2>
+		<p class="text">
+			為了讓人們有不同於書本的感受體驗，我們製作了劇場，觀眾可以旋轉兩側的把手，選擇自己想看的故事觀看，同時也可以觸摸下方舞台上的小人物，聽聽這個故事在講些甚麼。劇場結構是參考舊時代戲劇劇場的樣子下去製作，裝飾的部分是擷取自家中的舊飾品，以塑鋼土鑲嵌再上色的方式呈現。
+		</p>
+		<div class="bigpic">
+			<img src='asset/what/3.jpg'/>
+		</div>
+		<div class="pic">
+			<img src='asset/what/4.jpg'/>
+		</div>
+		<div class="pic">
+			<img src='asset/what/5.jpg'/>
+		</div>
+		<div class="pic">
+			<img src='asset/what/6.jpg'/>
+		</div>
+		`,
+		videoOpenning: null,
+		videoEndding: null
+	},
+	{
+		id: 19,
+		location: 'yesido',
+		name: 'Yes, I do',
+		person: ['王曉柔', '巫宜芸', '林玄蓁', '陳麒安'],
+		facepic:['person_1', 'person_2','person_3', 'person_4'],
+		info: '－窺現代愛情的文明病「工具人」',
+		content: `
+		<div class="bigpic">
+			<img src='asset/yesido/1.jpg'/>
+		</div>
+		<p class="text">
+			「我願意！為了你我什麼都願意做！考前整理重點給你、載你去男朋友的家、想吃什麼都幫你煮，只要你在我身邊都好...」<br/>
+			我們以工具人的能力為主打，並分為三大類─知識、勞力、金錢，以消費者的需求而雇用相對應功能的工具人。<br/>
+			我們替消費者設計一套工具人出任務時所需要的一組工具箱，工具箱內容物包含以情境圖方式呈現工具人出任務的作品集以及周邊商品。
+		</p>
+		<div class="pic">
+			<img src='asset/yesido/2.jpg'/>
+		</div>
+		<div class="pic">
+			<img src='asset/yesido/3.jpg'/>
+		</div>
+		<div class="pic">
+			<img src='asset/yesido/4.jpg'/>
+		</div>
+		<div class="pic">
+			<img src='asset/yesido/5.jpg'/>
+		</div>
+		`,
+		videoOpenning: null,
+		videoEndding: null
+	},
+]
+
+export default workData
